a50a2d1d0ad61871dbb019481234ef66f2044fd5
Chidiebere Uchechukwu <uchechidi9@gmail.com>
2020-10-31 15:37:16 +0100

    heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,8 @@ router.get('/', function (req, res) {
                 message: req.flash('loginError')
             }
 
-            //  res.status(200).send(data)
-            res.render("home")
+            res.status(200).send(data)
+            // res.render("home")
 
         }
     })
